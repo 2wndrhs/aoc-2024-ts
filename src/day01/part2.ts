e0c1b07ea619dab1019e3b89d12dac5acd862371
2wndrhs <2wndrhs@gmail.com>
2024-12-05 04:28:24 +0900

    feat: implement part 2 solution of day01

--- src/day01/part2.ts
+++ src/day01/part2.ts
@@ -1,5 +1,16 @@
 // Advent of Code - Day 1 - Part Two
 
 export function part2(leftList: number[], rightList: number[]): number {
-  return 0;
+  const frequencyMap = new Map();
+
+  rightList.forEach((number) => {
+    frequencyMap.set(number, (frequencyMap.get(number) ?? 0) + 1);
+  });
+
+  const totalScore = leftList.reduce((acc, cur) => {
+    const score = cur * (frequencyMap.get(cur) ?? 0);
+    return acc + score;
+  }, 0);
+
+  return totalScore;
 }
