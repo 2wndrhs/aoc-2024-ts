7203f243b8c994253263b6e6cd08a445bfd7b5b1
2wndrhs <2wndrhs@gmail.com>
2024-12-05 16:53:03 +0900

    test: move test files to __tests__ directory

--- src/day01/__tests__/day01.test.ts
+++ src/day01/__tests__/day01.test.ts
@@ -1,6 +1,6 @@
 import { describe, expect, test } from 'vitest';
-import { part1 } from './part1.js';
-import { part2 } from './part2.js';
+import { part1 } from '../part1.js';
+import { part2 } from '../part2.js';
 
 describe('Day 1', () => {
   const leftList = [3, 4, 2, 1, 3, 3];
--- src/day02/__tests__/day02.test.ts
+++ src/day02/__tests__/day02.test.ts
@@ -1,5 +1,6 @@
 import { describe, expect, test } from 'vitest';
-import { part1 } from './part1.js';
+import { part1 } from '../part1.js';
+import { part2 } from '../part2.js';
 
 describe('Day 2', () => {
   const data = [
@@ -13,5 +14,9 @@ describe('Day 2', () => {
 
   test('Part 1', () => {
     expect(part1(data)).toBe(2);
+  });
+
+  test('Part 2', () => {
+    expect(part2(data)).toBe(4);
   });
 });
