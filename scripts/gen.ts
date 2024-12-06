a27267ba92b4182c81a4c66b0287e489a3e185df
2wndrhs <2wndrhs@gmail.com>
2024-12-07 02:57:37 +0900

    feat: add script to fetch Advent of Code input file

---
+++ scripts/gen.ts
@@ -0,0 +1,52 @@
+import * as dotenv from 'dotenv';
+import { mkdir, writeFile } from 'fs/promises';
+
+const fetchInput = async (day: number, session: string): Promise<string> => {
+  const url = `https://adventofcode.com/2024/day/${day}/input`;
+  const headers = { cookie: `session=${session}` };
+
+  const response = await fetch(url, { headers });
+  return response.text();
+};
+
+const createInputFile = async (dayName: string, input: string) => {
+  const filePath = `src/${dayName}/resources/input.txt`;
+  await writeFile(filePath, input);
+};
+
+const createDirectories = async (dayName: string) => {
+  const resourcesPath = `src/${dayName}/resources`;
+  await mkdir(resourcesPath, { recursive: true });
+};
+
+(async function () {
+  // load environment variables
+  dotenv.config();
+
+  // check if exists one only argument
+  const dayName = process.argv[2];
+  if (process.argv.length !== 3) {
+    console.log('--- `pnpm run fetch-input` needs one only argument ---');
+    return;
+  }
+
+  // validate dayName format
+  const dayPattern = /^day(0[1-9]|1[0-9]|2[0-5])$/;
+  if (!dayPattern.test(dayName)) {
+    console.log('--- Invalid day format. Use dayXX (e.g., day01, day15) ---');
+    return;
+  }
+
+  const day = parseInt(dayName.slice(3));
+  const session = process.env.AOC_SESSION ?? '';
+
+  try {
+    // fetch input
+    const input = await fetchInput(day, session);
+    // create directories and save input
+    await createDirectories(dayName);
+    await createInputFile(dayName, input);
+  } catch (err) {
+    console.error(err);
+  }
+})();
