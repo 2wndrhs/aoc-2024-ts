7e9831e2bb3ef94e5b5fee773bb60e11cd97a722
2wndrhs <2wndrhs@gmail.com>
2024-12-19 14:52:32 +0900

    feat: implement Day 8 Part 1 solution

---
+++ src/day08/__tests__/day08.test.ts
@@ -0,0 +1,30 @@
+// Advent of Code - Day 8
+
+import { describe, expect, test } from 'vitest';
+import { part1 } from '../part1.js';
+import { part2 } from '../part2.js';
+
+describe('Day 8', () => {
+  const data = [
+    '............',
+    '........0...',
+    '.....0......',
+    '.......0....',
+    '....0.......',
+    '......A.....',
+    '............',
+    '............',
+    '........A...',
+    '.........A..',
+    '............',
+    '............',
+  ];
+
+  test('Part 1', () => {
+    expect(part1(data)).toBe(14);
+  });
+
+  test('Part 2', () => {
+    expect(part2(data)).toBe(0);
+  });
+});
---
+++ src/day08/part1.ts
@@ -0,0 +1,86 @@
+// Advent of Code - Day 8 - Part One
+
+type Position = {
+  row: number;
+  col: number;
+};
+
+const generatePositionCombinations = (
+  positionList: Position[],
+): [Position, Position][] => {
+  const combinations: [Position, Position][] = [];
+
+  for (let i = 0; i < positionList.length; i++) {
+    for (let j = i + 1; j < positionList.length; j++) {
+      combinations.push([positionList[i], positionList[j]]);
+    }
+  }
+
+  return combinations;
+};
+
+const isValidPosition = (position: Position, data: string[][]): boolean => {
+  return (
+    0 <= position.row &&
+    position.row < data.length &&
+    0 <= position.col &&
+    position.col < data[0].length
+  );
+};
+
+export function part1(input: string[]): number {
+  const data = input.map((row) => row.split(''));
+
+  const antennaMap = new Map<string, Position[]>();
+
+  data.forEach((row, rowIndex) => {
+    row.forEach((col, colIndex) => {
+      if (col !== '.') {
+        if (!antennaMap.has(col)) {
+          antennaMap.set(col, [
+            {
+              row: rowIndex,
+              col: colIndex,
+            },
+          ]);
+        } else {
+          antennaMap.get(col)!.push({
+            row: rowIndex,
+            col: colIndex,
+          });
+        }
+      }
+    });
+  });
+
+  const antinodeSet = new Set<string>();
+
+  Array.from(antennaMap.entries()).forEach(([antenna, positionList]) => {
+    const positionCombinations = generatePositionCombinations(positionList);
+
+    positionCombinations.forEach(([position1, position2]) => {
+      const rowDifference = position2.row - position1.row;
+      const colDifference = position2.col - position1.col;
+
+      const antinodePosition1 = {
+        row: position1.row - rowDifference,
+        col: position1.col - colDifference,
+      };
+
+      const antinodePosition2 = {
+        row: position2.row + rowDifference,
+        col: position2.col + colDifference,
+      };
+
+      if (isValidPosition(antinodePosition1, data)) {
+        antinodeSet.add(`${antinodePosition1.row},${antinodePosition1.col}`);
+      }
+
+      if (isValidPosition(antinodePosition2, data)) {
+        antinodeSet.add(`${antinodePosition2.row},${antinodePosition2.col}`);
+      }
+    });
+  });
+
+  return antinodeSet.size;
+}
